b11aaf43af36e4fa5af2b6196f153f209b95e56d
Christine <christine.altmann@gmx.com>
2019-08-12 14:56:02 +0100

    adding cookie to /login and redirecting to home

--- exercise/router.js
+++ exercise/router.js
@@ -1,34 +1,31 @@
-'use strict';
+"use strict";
 
-const { parse } = require('url');
-const { readFile } = require('fs');
-const path = require('path')
+const { parse } = require("url");
+const { readFile } = require("fs");
+const path = require("path");
 const notFoundPage = '<p style="font-size: 10vh; text-align: center;">404!</p>';
 
 module.exports = (req, res) => {
   switch (`${req.method} ${req.url}`) {
-    case 'GET /':
-      return readFile(
-        path.join(__dirname, 'index.html'),
-        (err, data) => {
-          res.writeHead(
-            200,
-            {
-              'Content-Type': 'text/html',
-              'Content-Length': data.length
-            }
-          );
-          return res.end(data);
-        }
-      );
+    case "GET /":
+      return readFile(path.join(__dirname, "index.html"), (err, data) => {
+        res.writeHead(200, {
+          "Content-Type": "text/html",
+          "Content-Length": data.length
+        });
+        return res.end(data);
+      });
+    case "POST /login":
+      res.writeHead(302, {
+        "Set-Cookie": "logged_in=true; HttpOnly; Max-Age=9000",
+        Location: "/"
+      });
+      return res.end();
     default:
-      res.writeHead(
-        404,
-        {
-          'Content-Type': 'text/html',
-          'Content-Length': notFoundPage.length
-        }
-      );
+      res.writeHead(404, {
+        "Content-Type": "text/html",
+        "Content-Length": notFoundPage.length
+      });
       return res.end(notFoundPage);
   }
-}
+};
